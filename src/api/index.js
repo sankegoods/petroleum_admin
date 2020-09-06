3232f2a4bc3854b119b5db4a629e237a25850e84
hole <your_registere953298392@qq.com>
2020-09-06 11:40:54 +0800

    油料模块

--- src/api/index.js
+++ src/api/index.js
@@ -6,4 +6,8 @@ export default {
     BaseUrl: "http://localhost:5000",
     menuActions: "/api/home/GetMenuInfo", //获取菜单数据
     login: "/api/home/login", //登陆
+    GetOilMaterialOrder: "/api/Order/GetOiMateOrder", //获取油料订单号
+    CreateOilMaterialOrder: "/api/Order/CreateOilMatOrder", //增添油料订单
+    UpdateOilMaterialOrder: "/api/Order/UpdateOilMatOrder", //修改油料订单
+    DeleteOilMaterialOrder: "/api/Order/DeleteOilMatOrder", //删除油料顶点
 }
--- src/views/Home/Oilseeds.vue
+++ src/views/Home/Oilseeds.vue
@@ -2,19 +2,17 @@
   <div>
     {{this.$route.name}}
     <el-row>
-      <el-col :span="15">
+      <el-col :span="24">
         <el-form :inline="true" :model="formInline" class="demo-form-inline">
-          <el-form-item label="审批人">
-            <el-input v-model="formInline.user" placeholder="审批人"></el-input>
+          <el-form-item label="姓名：">
+            <el-input v-model="formInline.ApplyPersonId" placeholder="姓名"></el-input>
           </el-form-item>
-          <el-form-item label="活动区域">
-            <el-select v-model="formInline.region" placeholder="活动区域">
-              <el-option label="区域一" value="shanghai"></el-option>
-              <el-option label="区域二" value="beijing"></el-option>
-            </el-select>
+          <el-form-item label="编号：">
+            <el-input v-model="formInline.noN" placeholder="编号"></el-input>
           </el-form-item>
           <el-form-item>
-            <el-button type="primary" @click="onSubmit">查询</el-button>
+            <el-button type="primary" @click="onSubmit(1)">查询</el-button>
+            <el-button type="primary" @click="addleEdit() ,drawer = true">增添</el-button>
           </el-form-item>
         </el-form>
       </el-col>
@@ -28,62 +26,242 @@
       style="width: 100%;text-align: center;"
     >
       <el-table-column label="订单信息">
-        <el-table-column prop="date" label="日期" width="180"></el-table-column>
-        <el-table-column prop="name" label="姓名" width="180"></el-table-column>
-        <el-table-column prop="address" label="地址"></el-table-column>
+        <el-table-column prop="noN" label="编号"></el-table-column>
+        <el-table-column prop="stfName" label="姓名"></el-table-column>
+        <el-table-column :formatter="formatDate" prop="createTime" label="创建时间"></el-table-column>
         <el-table-column label="操作">
           <template slot-scope="scope">
-            <el-button size="mini" @click="handleEdit(scope.$index, scope.row)">编辑</el-button>
-            <el-button size="mini" type="danger" @click="handleDelete(scope.$index, scope.row)">删除</el-button>
             <el-button
               size="mini"
               type="danger"
-              @click.native.prevent="deleteRow(scope.$index, tableData)"
-            >移除</el-button>
+              @click="handleEdit(scope.$index, scope.row) ,drawer = true"
+            >编辑</el-button>
+            <el-button
+              size="mini"
+              type="danger"
+              @click="handleDelete(scope.$index, scope.row,tableData)"
+            >删除</el-button>
           </template>
         </el-table-column>
       </el-table-column>
     </el-table>
+
+    <!--弹层-->
+    <el-drawer :title="title" :size="size" :visible.sync="drawer" :direction="direction">
+      <el-form ref="form" :model="form" label-width="80px">
+        <el-form-item label="申请人">
+          <el-input v-model="form.ApplyPersonId" :disabled="isdisabled"></el-input>
+        </el-form-item>
+        <el-form-item label="油料规格">
+          <el-input v-model="form.OilSpec"></el-input>
+        </el-form-item>
+        <el-form-item label="油罐容积">
+          <el-input v-model="form.Volume"></el-input>
+        </el-form-item>
+        <el-form-item label="剩余油料">
+          <el-input v-model="form.Surplus"></el-input>
+        </el-form-item>
+        <el-form-item label="日均销量">
+          <el-input v-model="form.DayAvg"></el-input>
+        </el-form-item>
+        <el-form-item label="需要量">
+          <el-input v-model="form.NeedAmount"></el-input>
+        </el-form-item>
+        <el-form-item label="备注">
+          <el-input v-model="form.Remark"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" @click="onSubmit(2)">{{this.buttonName}}</el-button>
+          <el-button @click="cancelForm">取 消</el-button>
+        </el-form-item>
+      </el-form>
+    </el-drawer>
   </div>
 </template>
 <script>
+import { request } from "../../network/request.js";
+import url from "../../api";
 export default {
   data() {
     return {
-      tableData: [
-        {
-          date: "2016-05-03",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1518 弄",
-        },
-        {
-          date: "2016-05-02",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1518 弄",
-        },
-      ],
+      tableData: [],
       formInline: {
-        user: "",
-        region: "",
+        ApplyPersonId: "",
+        noN: "",
+      },
+      direction: "rtl",
+      isdisabled: true,
+      drawer: false,
+      size: "30%",
+      title: "编辑",
+      buttonName: "",
+      form: {
+        // 弹框中表单数据
+        ApplyPersonId: "",
+        Remark: "",
+        Id: "",
+        OilSpec: "", //油料规格
+        Volume: "", //油罐容积
+        Surplus: "", //剩余油料
+        DayAvg: "", //日均销售量
+        NeedAmount: "", //需要量
+        DetailID: "", //订单详情ID
       },
     };
   },
+  activated() {
+    this.logInfo();
+  },
+  deactivated() {
+    console.log("不活跃");
+  },
   methods: {
+    //查看修改
     handleEdit(index, row) {
-      console.log(index);
-      console.log(row);
+      this.title = "修改";
+      this.size = "30%";
+      this.buttonName = "立即修改";
+      this.direction = "rtl";
+      this.isdisabled = true;
+      this.form.ApplyPersonId = row.applyPersonId;
+      this.form.Remark = row.remark;
+      this.form.OilSpec = row.oilSpec;
+      this.form.Volume = row.volume;
+      this.form.Surplus = row.surplus;
+      this.form.DayAvg = row.dayAvg;
+      this.form.NeedAmount = row.needAmount;
+      this.form.DetailID = row.detailID;
+      this.form.Id = row.id;
+      console.log(this.form);
     },
-    handleDelete(index, row) {
-      console.log(index);
-      console.log(row);
+    //添加
+    addleEdit() {
+      this.direction = "ttb";
+      this.isdisabled = false;
+      this.title = "添加";
+      this.size = "60%";
+      this.buttonName = "添加";
+      this.form.ApplyPersonId = "";
+      this.form.Remark = "";
+      this.form.OilSpec = "";
+      this.form.Volume = "";
+      this.form.Surplus = "";
+      this.form.DayAvg = "";
+      this.form.NeedAmount = "";
+      this.form.DetailID = "";
     },
-    deleteRow(index, row) {
-      console.log(index);
-      console.log(row);
-      row.splice(index, 1);
+    formatDate(row, column) {
+      // 获取单元格数据
+      let data = row["createTime"];
+      if (this.tableData == null) {
+        return "";
+      }
+      let dt = new Date(data);
+      return (
+        dt.getFullYear() +
+        "-" +
+        (dt.getMonth() + 1) +
+        "-" +
+        dt.getDate() +
+        " " +
+        dt.getHours() +
+        ":" +
+        dt.getMinutes() +
+        ":" +
+        dt.getSeconds()
+      );
     },
-    onSubmit() {
-      console.log(this.formInline);
+    //删除
+    handleDelete(index, row, info) {
+      request({
+        url: url.DeleteOilMaterialOrder,
+        params: { Id: row.id, DetailID: row.detailID },
+      })
+        .then((res) => {
+          if (res.data) {
+            this.$message({
+              message: "删除成功",
+              type: "success",
+            });
+            info.splice(index, 1);
+          } else {
+            this.$message.error("删除异常 稍后再试");
+          }
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+      // console.log(index);
+      // console.log(row);
+      // console.log(info);
+    },
+    //取消弹框
+    cancelForm() {
+      this.drawer = false;
+    },
+    onSubmit(types, then) {
+      switch (types) {
+        case 1:
+          this.$message({
+              message: "功能稍后开放",
+              type: "success",
+            });
+          // console.log(this.formInline)
+          break;
+        case 2:
+          if (this.buttonName == "添加") {
+            request({
+              url: url.CreateOilMaterialOrder,
+              method: "Post",
+              data: {
+                ApplyPersonId: this.form.ApplyPersonId,
+                Remark: this.form.Remark,
+                OilSpec: this.form.OilSpec,
+                Volume: Number(this.form.Volume),
+                Surplus: Number(this.form.Surplus),
+                DayAvg: Number(this.form.DayAvg),
+                NeedAmount: Number(this.form.NeedAmount),
+              },
+            })
+              .then((res) => {
+                this.drawer = false;
+                this.logInfo();
+                // console.log(res.data);
+              })
+              .catch((err) => {
+                console.log(err);
+              });
+          } else {
+            request({
+              url: url.UpdateOilMaterialOrder,
+              method: "Post",
+              data: this.form,
+            })
+              .then((res) => {
+                if (res.data) {
+                  this.drawer = false;
+                  this.logInfo();
+                }
+              })
+              .catch((err) => {
+                console.log(err);
+              });
+          }
+          break;
+        default:
+          break;
+      }
+    },
+    logInfo() {
+      request({
+        url: url.GetOilMaterialOrder,
+      })
+        .then((res) => {
+          this.tableData = res.data;
+        })
+        .catch((err) => {
+          console.log(err);
+        });
     },
   },
 };
